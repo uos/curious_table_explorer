3971b15945744c4a9e0827f64045b593956513b6
Michael Görner <mgoerner@uos.de>
2014-08-06 18:16:07 +0200

    add uniform_color_distribution class

---
+++ include/uniform_color_distribution.h
@@ -0,0 +1,24 @@
+#ifndef _UNIFORM_COLOR_DISTRIBUTION_H_
+#define _UNIFORM_COLOR_DISTRIBUTION_H_
+
+#include <random>
+#include <std_msgs/ColorRGBA.h>
+
+class uniform_color_distribution {
+public:
+	uniform_color_distribution() : uni_dist(0.0, 1.0){}
+
+	std_msgs::ColorRGBA operator()(std::default_random_engine& engine){
+		std_msgs::ColorRGBA color;
+		color.a= 1;
+		color.r= this->uni_dist(engine);
+		color.g= this->uni_dist(engine);
+		color.b= this->uni_dist(engine);
+		return color;
+	}
+
+private:
+	std::uniform_real_distribution<double> uni_dist;
+};
+
+#endif
