8cfdbf46d7d93c908dfa101e661cb4aaaa212fcf
Michael Görner <mgoerner@uos.de>
2014-10-07 11:40:52 +0200

    tabletop segmentation: keep camera frame & crop floor
    Move away from Cropper because it doesn't return indices

--- python/my_tabletop_segmentation.py
+++ python/my_tabletop_segmentation.py
@@ -30,12 +30,13 @@ class TableTopSegmentationServer:
 		
 	def create_plasm(self):
 		self.plasm= ecto.Plasm()
-		graph= []
 
 		cloud_sub= ecto_sensor_msgs.Subscriber_PointCloud2("cloud_sub", topic_name= '/kinect/depth_registered/points', queue_size= 1)
 		msg2cloud= ecto_pcl_ros.Message2PointCloud("msg2cloud")
-		floor_cropper= ecto_pcl.Cropper(z_min= .25)
 		cloud_to_map= my_ecto_cells.CloudReframer(target_frame= '/map', timeout= 1.0)
+		floor_cropper= my_ecto_cells.PassThroughIndices(filter_field_name= "z", filter_limit_min= .20)
+		extract_indices_floor= ecto_pcl.ExtractIndices()
+
 		voxel_grid= ecto_pcl.VoxelGrid("voxel_grid", leaf_size=.008)
 		normals= ecto_pcl.NormalEstimation("normals", radius_search= .02, k_search= 0)
 		planar_segmentation= ecto_pcl.SACSegmentationFromNormals("planar_segmentation",
@@ -46,28 +47,32 @@ class TableTopSegmentationServer:
 		inlier_projection= ecto_pcl.ProjectInliers("inlier_projection", model_type= ecto_pcl.SACMODEL_NORMAL_PLANE)
 		convex_table= ecto_pcl.ConvexHull("convex_table", dimensionality= 2)
 
-		#TODO: Why are up/down inverse? -> Set viewpoint in pcl class
-		extract_table_content= ecto_pcl.ExtractPolygonalPrismData("extract_table_content", height_min=-0.5, height_max=-.02)
+		extract_table_content= ecto_pcl.ExtractPolygonalPrismData("extract_table_content", height_min=.02, height_max=.5)
 
 		extract_table_content_indices= ecto_pcl.ExtractIndices("extract_table_content_indices", negative= False)
 		cloud2msg= ecto_pcl_ros.PointCloud2Message()
 		table_content_cloud_pub= ecto_sensor_msgs.Publisher_PointCloud2("table_content_cloud_pub", topic_name= '/table_content')
 
-		graph+= [
+		graph= [
 			cloud_sub[:] >> msg2cloud[:],
+
 			msg2cloud[:] >> cloud_to_map[:],
 			cloud_to_map[:] >> floor_cropper[:],
-			floor_cropper[:] >> voxel_grid[:],
+			floor_cropper[:] >> extract_indices_floor["indices"],
+			msg2cloud[:] >> extract_indices_floor["input"],
+
+			extract_indices_floor[:] >> voxel_grid[:],
 			voxel_grid[:] >> normals[:],
 			voxel_grid[:] >> planar_segmentation["input"],
 			normals[:]    >> planar_segmentation["normals"],
+
 			planar_segmentation["inliers"] >> extract_table_indices["indices"],
 			voxel_grid[:] >> extract_table_indices["input"],
 			extract_table_indices[:] >> inlier_projection["input"],
 			planar_segmentation["model"] >> inlier_projection["model"],
 			inlier_projection[:] >> convex_table[:],
 			convex_table[:] >> extract_table_content["planar_hull"],
-			cloud_to_map[:] >> extract_table_content["input"],
+			msg2cloud[:] >> extract_table_content["input"],
 			extract_table_content[:] >> extract_table_content_indices["indices"],
 			msg2cloud[:] >> extract_table_content_indices["input"],
 			extract_table_content_indices[:] >> cloud2msg[:],
