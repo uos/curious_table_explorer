e932c774c51ba22acabd6152dc874b7d6b8bdad9
Michael Görner <mgoerner@uos.de>
2015-06-13 15:22:14 +0200

    surround_point should allow to pass delta

--- python/surround_point.py
+++ python/surround_point.py
@@ -13,16 +13,20 @@ if __name__ == '__main__':
 	rospy.init_node('surround_client')
 
 	if len(sys.argv) != 3 and len(sys.argv) != 4:
-		print "usage: surround_client.py <x> <y> [frame_id]"
-		print "will surround the point (x,y,0) in /map or [frame_id]"
+		print "usage: surround_client.py <x> <y> [frame_id] [delta]"
+		print "will surround the point (x,y,0) in /map or [frame_id] with delta(rad) between two poses"
 		exit(1)
 
 	x= float(sys.argv[1])
 	y= float(sys.argv[2])
 
 	frame_id= "/map"
-	if len(sys.argv) == 4:
+	if len(sys.argv) > 3:
 		frame_id= sys.argv[3]
+
+	delta= 0.5
+	if len(sys.argv) > 4:
+		delta= float(sys.argv[4])
 
 	client= actionlib.SimpleActionClient('/surround_point', SurroundPointAction)
 	client.wait_for_server()
@@ -31,7 +35,8 @@ if __name__ == '__main__':
 			point= PointStamped(
 				Header(frame_id= frame_id),
 				Point(x,y,0.0)
-			)
+			),
+			delta= delta
 		)
 	)
 	client.wait_for_result()
